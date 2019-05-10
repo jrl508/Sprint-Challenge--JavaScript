d1cff8a923689e0bc8c2c26f1e98ce2d113c64e8
jrl508 <jrl47@yahoo.com>
2019-05-10 11:53:44 -0400

    initial commit, begun work on object-arrays.js challenges

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,30 +5,46 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+class dinosaur {
+  constructor(name, diet, weight,length,period){
+    this.name = name,
+    this.diet = diet,
+    this.weight = weight,
+    this.length = length,
+    this.period = period
+  }
 
+  roar(){
+    return (`RAWERSRARARWERSARARARRRR!`)
+  }
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const sharpTooth = new dinosaur("tyrannosaurus", "carnivorous", "7000kg", "12m", "Late Cretaceous")
+console.log(sharpTooth)
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const spike = new dinosaur("stegosaurus", "herbivorous", "2000kg", "9m", "Late Jurassic")
+console.log(spike)
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const blue = new dinosaur("velociraptor", "carnivorous", "15kg", "1.8m", "Late Cretaceous")
+console.log(blue)
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(sharpTooth.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(blue.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(spike.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(sharpTooth.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(sharpTooth.roar());
 
 
 // ==== Arrays ====
